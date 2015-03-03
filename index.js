0ecb2718234d188fd377960878f4eb878aa29e07
Flávio Sousa <flavio.jsousa@gmail.com>
2015-03-03 18:41:01 +0000

    namespace stuff

--- index.js
+++ index.js
@@ -7,46 +7,79 @@ var WebSocketServer = require('ws').Server;
 var peerServer = PeerServer({port: 9000, path: '/myapp'});
 var wss = new WebSocketServer({port: 9001});
 
-var peers = [];
-peerServer.on('connection', function (id) {
-  console.log('Peer connected with id:', id);
-  peers.push(id);
+var peers = {};
+peerServer.on('connection', function (namespacedId) {
+  //groups peer ids according to their namespaces
+  // { 
+  //   default: [ 
+  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787',
+  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787'
+  //    ],
+  //   otherexperiment: [ 
+  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787',
+  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787'
+  //    ]
+  // }
 
-  signalMaster(); 
+  console.log('Peer connected with id:', namespacedId); 
+
+  var prefix = getPrefix(namespacedId);
+  var id = getId(namespacedId);
+  
+  //if the prefix doesn't exist create it
+  if (!peers.hasOwnProperty(prefix))
+    peers[prefix] = [];
+
+  peers[prefix].push(namespacedId);
+
+  console.log(peers);
+
+  signalMaster(prefix); 
 });
 
-peerServer.on('disconnect', function (id) {
-  console.log('Peer %s disconnected', id);
-  var i = peers.indexOf(id);
-  peers.splice(i, 1);
+// peerServer.on('disconnect', function (id) {
+//   console.log('Peer %s disconnected', id);
+//   var i = peers.indexOf(id);
+//   peers.splice(i, 1);
 
-  signalMaster();
-});
+//   // signalMaster();
+// });
 
-var masterSocket = null;
+var masterSockets = {};
   
 wss.on('connection', function(socket) {
-  //First socket is master socket
-  if (!masterSocket){
-    masterSocket = socket;
-    signalMaster();
 
-    //handle Master Socket disconnet
-    socket.on('close', function() {
-      masterSocket = null;
-      console.log('Master disconnected.');
-    });
-  }
+  socket.on('message', function(namespacedId){
+    var prefix = getPrefix(namespacedId);
+    var id = getId(namespacedId);
 
+    if (masterSockets[prefix] === undefined){
+      masterSockets[prefix] = socket;
+      signalMaster(prefix);  
+
+      socket.on('close', function() {
+        masterSockets[prefix] = undefined;
+        console.log('Master' + prefix + 'disconnected.');
+      });
+    }
+  });
 });
 
-function signalMaster(){
-  if (!masterSocket || peers.length === 0 )
+function signalMaster(prefix){  
+  if (!masterSockets[prefix] || peers[prefix].length === 0 )
     return;
 
   console.log('Sending peer list  to Master');
-  masterSocket.send(JSON.stringify(peers), function ack(error) {
+  masterSockets[prefix].send(JSON.stringify(peers[prefix]), function ack(error) {
     if (error)
       console.log('Error sending data to Master client.');
   });
+}
+
+function getPrefix(id){
+  return id.split('-')[0];  
+}
+
+function getId(id){
+  return id.split('-')[1];  
 }
--- static.js
+++ static.js
@@ -1,12 +1,11 @@
-var static = require('node-static');
-var http = require('http');
+var express = require('express');
+var app = express();
+app.use('/:prefix', express.static('.'));
+app.use('/', express.static('.'));
+module.exports = function start(port) {
 
-module.exports = function (port) {
-  
-  var file = new(static.Server)();
-
-  http.createServer(function (req, res) {
-    file.serve(req, res);
-  }).listen(port);  
+  server = app.listen(port, function () {
+    console.log('Basic-ss live at', port);      
+  });
 
 };
