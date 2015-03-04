ca7d0a94c273413468e2a7d4503064d0517744e4
Flávio Sousa <flavio.jsousa@gmail.com>
2015-03-04 14:48:24 +0000

    using express only

--- index.js
+++ index.js
@@ -1,101 +1,67 @@
+//groups peer ids according to their namespaces
+// { 
+//   default: [ 
+//     'default-4a78940f2f1f8b03195103a3baafc0db1425405629865787',
+//     'default-4a78940f2f1f8b03195103a3baafc0db1425405629865787'
+//    ],
+//   otherexperiment: [ 
+//     'otherexperiment-4a78940f2f1f8b03195103a3baafc0db1425405629865787',
+//     'otherexperiment-4a78940f2f1f8b03195103a3baafc0db1425405629865787'
+//    ]
+// }
+
+var http = require('http');
 var express = require('express');
 var ExpressPeerServer = require('peer').ExpressPeerServer;
-var WebSocketServer = require('ws').Server;
+var _ = require('underscore');
 
 var app = express();
-app.use('/:prefix', express.static(__dirname + 'client'));
-app.use('/', express.static('client'));
-
-var server = require('http').createServer(app);
+var server = http.createServer(app);
 
 var options = {
-    debug: false
+    debug: true
 };
+var expressPeerServer = ExpressPeerServer(server, options);
 
-var expresspeerserver = ExpressPeerServer(server, options);
+app.use('/api', expressPeerServer);
 
-app.use('/api', expresspeerserver);
+app.use('/:prefix', express.static(__dirname + '/client'));
+app.use('/list/:prefix', function (req, res) {
+
+  var peers = expressPeerServer._clients.peerjs;
+
+  if (!peers) 
+    return res.json([]);
+
+  var peersKeys = Object.keys(peers); 
+  var requestPeers = [];
+
+  var prefix = req.params.prefix; 
+
+  _.filter(peersKeys, function (peerId) {
+    var peerPrefix = getPrefix(peerId);
+
+    if (peerPrefix === prefix)
+      requestPeers.push(peerId);
+  });
+
+  res.json(requestPeers);
+
+});
 
 var port = process.env.PORT || 5000;
 server.listen(port, function () {
-  console.log('Basic-ss live at', port);      
+  console.log('Not-so-basic-ss live at', port);      
 });
 
-var wss = new WebSocketServer({port: 9001});
-
-var peers = {};
-expresspeerserver.on('connection', function (namespacedId) {
-  //groups peer ids according to their namespaces
-  // { 
-  //   default: [ 
-  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787',
-  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787'
-  //    ],
-  //   otherexperiment: [ 
-  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787',
-  //     '4a78940f2f1f8b03195103a3baafc0db1425405629865787'
-  //    ]
-  // }
-
-  console.log('Peer connected with id:', namespacedId); 
-
-  var prefix = getPrefix(namespacedId);
-  var id = getId(namespacedId);
-  
-  //if the prefix doesn't exist create it
-  if (!peers.hasOwnProperty(prefix))
-    peers[prefix] = [];
-
-  peers[prefix].push(namespacedId);
-
-  console.log(peers);
-
-  signalMaster(prefix); 
+expressPeerServer.on('connection', function (id) {
+  console.log('Peer connected with id:', id); 
 });
 
-// expresspeerserver.on('disconnect', function (id) {
-//   console.log('Peer %s disconnected', id);
-//   var i = peers.indexOf(id);
-//   peers.splice(i, 1);
-
-//   // signalMaster();
-// });
-
-var masterSockets = {};
-  
-wss.on('connection', function(socket) {
-
-  socket.on('message', function(namespacedId){
-    var prefix = getPrefix(namespacedId);
-    var id = getId(namespacedId);
-
-    if (masterSockets[prefix] === undefined){
-      masterSockets[prefix] = socket;
-      signalMaster(prefix);  
-
-      socket.on('close', function() {
-        masterSockets[prefix] = undefined;
-        console.log('Master' + prefix + 'disconnected.');
-      });
-    }
-  });
+expressPeerServer.on('disconnect', function (id) {
+  console.log('Peer %s disconnected', id);
 });
-
-function signalMaster(prefix){  
-  if (!masterSockets[prefix] || peers[prefix].length === 0 )
-    return;
-
-  console.log('Sending peer list  to Master');
-  masterSockets[prefix].send(JSON.stringify(peers[prefix]), function ack(error) {
-    if (error)
-      console.log('Error sending data to Master client.');
-  });
-}
 
 function getPrefix(id){
   return id.split('-')[0];  
-}
-
-function getId(id){
-  return id.split('-')[1];  
 }
